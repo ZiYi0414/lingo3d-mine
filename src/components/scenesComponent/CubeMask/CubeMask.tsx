c58f9aee7af5736fb36e604c3441af0560b5d247
LZY <170280176@qq.com>
2022-04-08 15:27:44 +0800

    feat:我也不知道

--- src/components/scenesComponent/CubeMask/CubeMask.tsx
+++ src/components/scenesComponent/CubeMask/CubeMask.tsx
@@ -1,9 +1,5 @@
-import SkyMask from './component/SkyMask'
-import BackCube from './component/BackCube'
 import GroundCube from './component/GroundCube'
-import FontCube from './component/FontCube'
-import LeftCube from './component/LeftCube'
-import RightCube from './component/RightCube'
+
 
 const CubeMask = () => {
   return (
